a1c5e189abc3dcac3f83306b93280f1c719702b8
gilyeon00 <12venus08@naver.com>
2023-05-06 14:39:42 +0900

    [#1]Feat: MainBanner 생성

---
+++ frontend/src/components/mainBanner/MainBanner.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './mainBanner.css'
+
+const MainBanner = () => {
+    return (
+        <div className='mainBanner'>
+            <div className='mammyMsg'>
+                <p>우리 딸 ~ 오늘도 사랑해~~❤️</p>
+            </div>
+            <div className='slideBanner'>
+
+            </div>
+        </div>
+    );
+};
+
+export default MainBanner;
--- frontend/src/pages/KidMain.jsx
+++ frontend/src/pages/KidMain.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import MainBanner from '../components/mainBanner/MainBanner';
 
 const KidMain = () => {
     return (
         <div>
             <h1>ggg</h1>
+            <MainBanner/>
         </div>
     );
 };
