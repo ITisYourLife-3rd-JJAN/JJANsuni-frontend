711ca0850b294ae1f03b37450f721ae477de7396
gilyeon00 <12venus08@naver.com>
2023-05-06 14:26:00 +0900

    [#1]Feat: Header 컴포넌트 생성

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Header from './components/Header';
+import Header from './components/header/Header';
 import KidMain from './pages/KidMain';
 import ParentMain from './pages/ParentMain';
 import Intro from './pages/Intro';
---
+++ frontend/src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <p>JJAN순이의 경제여행</p>
+            <p>000 아이님✨</p>
+            <button>로그아웃</button>
+        </div>
+    );
+};
+
+export default Header;
