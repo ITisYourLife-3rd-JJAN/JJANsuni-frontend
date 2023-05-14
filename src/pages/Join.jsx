f45719e16035fc704c4355be8bc366e4c59f8f15
seungukNoh <dnrdl8255@gmail.com>
2023-05-15 01:47:37 +0900

    회원가입 hmtl,css 초안 작성. 로그인도 해봄

--- src/pages/Join.jsx
+++ src/pages/Join.jsx
@@ -1,9 +1,48 @@
 import React from 'react';
+import './join.css'
 
 const Join = () => {
     return (
-        <div>
-            
+        <div className='join'>
+            <div className='joinSub'>
+
+                <div className='joinSlogan'>
+                    <p>세 살 버릇 여든까지!</p>
+                </div>
+
+                <div className='joinImage'>
+                    <img ClassName="joinParent " src="img/parentJoin.png"></img>
+                </div>
+                
+
+                <div className="joinInput">
+                    <form action="#" method="post">
+                        <p>
+                            <label for="username">이름</label><br/><br/>
+                            <input type="text" id="username" required></input>
+                        </p>
+                        <p>
+                            <label for="email">이메일</label><br/><br/>
+                            <input type="email" id="email" required></input>
+                        </p>
+                        <p>
+                            <label for="userpw">비밀번호</label><br/><br/>
+                            <input type="password" id="userpw" required></input>
+                        </p>
+                        <p>
+                            <label for="userpwCheck">비밀번호 확인</label><br/><br/>
+                            <input type="password" id="userpwCheck" required></input>
+                        </p>
+
+                        <p>
+                        <button>다음</button>
+                        </p>
+                    </form>
+                </div>
+
+              
+            </div>
+
         </div>
     );
 };
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,39 @@
 import React from 'react';
+import './login.css'
 
 const Login = () => {
     return (
-        <div>
-            
+        <div className='login'>
+            <div className='loginSub'>
+
+                <div className='joinSlogan'>
+                    <p>오늘도 여행을 떠나볼까요?</p>
+                </div>
+
+                <div className='loginImage'>
+                    <img ClassName="trip" src="img/trip.png"></img>
+                </div>
+                
+
+                <div className="loginInput">
+                    <form action="#" method="post">
+                        <p>
+                            <label for="email">이메일</label><br/><br/>
+                            <input type="email" id="email" required></input>
+                        </p>
+                        <p>
+                            <label for="userpw">비밀번호</label><br/><br/>
+                            <input type="password" id="userpw" required></input>
+                        </p>
+                        <p>
+                        <button>로그인</button>
+                        </p>
+                    </form>
+                </div>
+
+              
+            </div>
+
         </div>
     );
 };
