bd19b46a4b16f4055270b279b53e20fc90b162bd
gilyeon00 <12venus08@naver.com>
2023-05-14 23:34:10 +0900

    [#9]Design: publish frame of Map page

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -7,8 +7,61 @@ const Map = () => {
             <img
                 src={`${process.env.PUBLIC_URL}/assets/images/map/map_background.png`}
                 alt=''
-                className='background-image'
+                className='map-background-image'
             />
+            <div className='map-box map-box-1'>
+                <p>7/7</p>
+                <img
+                    src={`${process.env.PUBLIC_URL}/assets/images/map/island1.png`}
+                    alt=''
+                    className='map map-1-image'
+                />
+            </div>
+
+            <div className='map-box map-box-2'>
+                <p>1/7</p>
+                <img
+                    src={`${process.env.PUBLIC_URL}/assets/images/map/island2.png`}
+                    alt=''
+                    className='map map-2-image'
+                />
+             </div>
+            
+            <div className='map-box map-box-3'>
+                <p>0/7</p>
+                <img
+                    src={`${process.env.PUBLIC_URL}/assets/images/map/island3.png`}
+                    alt=''
+                    className='map map-3-image'
+                />
+             </div>
+             
+            <div className='map-box map-box-4'>
+                <p>0/7</p>
+                <img
+                    src={`${process.env.PUBLIC_URL}/assets/images/map/island4.png`}
+                    alt=''
+                    className='map map-4-image'
+                />
+             </div>
+            
+            <div className='map-box map-box-5'>
+                <p>0/7</p>
+                <img
+                    src={`${process.env.PUBLIC_URL}/assets/images/map/island5.png`}
+                    alt=''
+                    className='map map-5-image'
+                />
+             </div>
+             
+            <div className='map-box map-box-6'>
+                <p>0/7</p>
+                <img
+                    src={`${process.env.PUBLIC_URL}/assets/images/map/island6.png`}
+                    alt=''
+                    className='map map-6-image'
+                />
+             </div>
         </div>
     );
 };
