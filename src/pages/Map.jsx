807962ea175952dc235aef58ff742d7d2262860f
gilyeon00 <12venus08@naver.com>
2023-05-14 22:35:52 +0900

    [#9]Design: apply map background image

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import './css/map.css'
 
 const Map = () => {
     return (
-        <div>
-            
+        <div className='map-container'>  
+            <img
+                src={`${process.env.PUBLIC_URL}/assets/images/map/map_background.png`}
+                alt=''
+                className='background-image'
+            />
         </div>
     );
 };
