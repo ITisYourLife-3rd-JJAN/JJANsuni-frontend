b6b516477307c9f226ac91482a7851d793aa670d
gilyeon00 <12venus08@naver.com>
2023-05-14 18:24:27 +0900

    [#7]Feat: 부모일 때 MainBanner 색상 변경

--- src/components/mainBanner/MainBanner.jsx
+++ src/components/mainBanner/MainBanner.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import './mainBanner.css'
 import SimpleSlider from '../slider/Slider';
 
-const MainBanner = () => {
+const MainBanner = ({bgColor = '#CDFF5C'}) => {
     return (
-        <div className='mainBanner'>
+        <div className='mainBanner' style={{backgroundColor: bgColor}}>
             <div className='mammyMsg'>
                 <p>우리 딸 ~ 오늘도 사랑해~~❤️</p>
             </div>
