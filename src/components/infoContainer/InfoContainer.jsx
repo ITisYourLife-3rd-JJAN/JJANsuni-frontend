fa4d62f8448329219bdf574f73317868736b1651
Subny <richsubin411@gmail.com>
2023-05-15 00:20:17 +0900

    [#6]Feat: 헤더 아래에 오는 여러 선택탭 추가

--- src/components/infoContainer/InfoContainer.jsx
+++ src/components/infoContainer/InfoContainer.jsx
@@ -4,8 +4,15 @@ import './infoContainer.css';
 const InfoContainer = () => {
     return (
         <div className='infoContainer'>
-            <div >
-                
+            <div className='infoTab' >
+            <img src={`${process.env.PUBLIC_URL}/assets/images/credit.png`} alt='' />
+            카드내역
+            </div>
+            <div className='infoTab'>
+            아이 현재 잔액 : 35,000원            
+            </div>
+            <div className='infoTab'>
+            정길연 아이         
             </div>
         </div>
     );
