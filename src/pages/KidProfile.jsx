b7cbfc2753ce557050123e42f12e3f4ccaab988b
gilyeon00 <12venus08@naver.com>
2023-05-15 20:55:04 +0900

    [#18]Feat: Kid my Profile card 구현

--- src/pages/KidProfile.jsx
+++ src/pages/KidProfile.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ProfileBackground from '../components/profile/ProfileBackground';
+import MyBigCard from '../components/profile/MyBigCard';
 
 const KidProfile = () => {
     return (
-        <div>
+        <div className='profile-cards-container'>
             <ProfileBackground/>
+            <MyBigCard/>
         </div>
     );
 };
