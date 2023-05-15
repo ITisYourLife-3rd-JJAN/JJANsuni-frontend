46af3a18b087936574a2e8aca9070e5398dd3afe
gilyeon00 <12venus08@naver.com>
2023-05-15 12:26:11 +0900

    [#11]Design: 문제 현황(정답여부) 1차 구현

--- src/pages/MissionList.jsx
+++ src/pages/MissionList.jsx
@@ -1,4 +1,4 @@
-import {React, userState} from 'react';
+import {React, useState} from 'react';
 import './css/missionList.css'
 import { useParams } from 'react-router-dom';
 import MapBackground from '../components/MapBackground';
@@ -7,11 +7,20 @@ import './css/missionList.css';
 
 const MissionList = () => {
     const { mapId } = useParams();
+    const variable_o = 'o';
+    const variable_x = 'x';
+    const answer_img_o= `${process.env.PUBLIC_URL}/assets/images/map/${variable_o}_answer.png`;
+    const answer_img_x= `${process.env.PUBLIC_URL}/assets/images/map/${variable_x}_answer.png`;
     
     return (
         <div>
             <MapBackground mapId={mapId}/>
             <div className= 'map-box absol1'>
+                <img
+                    src={answer_img_x}
+                    alt=''
+                    className='answer-img'
+                />
                 <img
                     src={`${process.env.PUBLIC_URL}/assets/images/map/island1.png`}
                     alt=''
@@ -19,6 +28,11 @@ const MissionList = () => {
                 />
              </div>
              <div className= 'map-box absol2'>
+                 <img
+                    src={answer_img_o}
+                    alt=''
+                    className='answer-img'
+                />
                 <img
                     src={`${process.env.PUBLIC_URL}/assets/images/map/island1.png`}
                     alt=''
@@ -27,12 +41,22 @@ const MissionList = () => {
              </div>
              <div className= 'map-box absol3'>
                 <img
+                    src={answer_img_x}
+                    alt=''
+                    className='answer-img'
+                />
+                <img
                     src={`${process.env.PUBLIC_URL}/assets/images/map/island1.png`}
                     alt=''
                     className='map map-image-1'
                 />
              </div>
              <div className= 'map-box absol4'>
+                <img
+                    src={answer_img_o}
+                    alt=''
+                    className='answer-img'
+                />
                 <img
                     src={`${process.env.PUBLIC_URL}/assets/images/map/island1.png`}
                     alt=''
@@ -41,6 +65,11 @@ const MissionList = () => {
              </div>
              <div className= 'map-box absol5'>
                 <img
+                    src={answer_img_o}
+                    alt=''
+                    className='answer-img'
+                />
+                <img
                     src={`${process.env.PUBLIC_URL}/assets/images/map/island1.png`}
                     alt=''
                     className='map map-image-1'
@@ -48,12 +77,22 @@ const MissionList = () => {
              </div>
              <div className= 'map-box absol6'>
                 <img
+                    src={answer_img_o}
+                    alt=''
+                    className='answer-img'
+                />
+                <img
                     src={`${process.env.PUBLIC_URL}/assets/images/map/island1.png`}
                     alt=''
                     className='map map-image-1'
                 />
              </div>
              <div className= 'map-box absol7'>
+                <img
+                    src={answer_img_x}
+                    alt=''
+                    className='answer-img'
+                />
                 <img
                     src={`${process.env.PUBLIC_URL}/assets/images/map/island1.png`}
                     alt=''
