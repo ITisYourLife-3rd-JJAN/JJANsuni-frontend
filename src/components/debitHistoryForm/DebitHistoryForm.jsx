6ee1dd471f32850243cbb2c61db1f8fb1d6e8772
Subny <richsubin411@gmail.com>
2023-05-15 19:46:38 +0900

    [#15]Feat: 이체내역 component 부모 아이 판별해서 배경색 변경

--- src/components/debitHistoryForm/DebitHistoryForm.jsx
+++ src/components/debitHistoryForm/DebitHistoryForm.jsx
@@ -7,14 +7,18 @@ const DebitHistoryForm = ({isParent}) => {
     return (
         <div className='debitHistory' style={{backgroundColor:backgroundColor}}>
             <table className='debitTable'>
-                <thead>
-                    <th>이체 날짜</th>
-                    <th>{isParent? '받는 사람' : '보내는 사람'}</th>
-                    <th>이체 메시지</th>
-                    <th>이체 금액</th>
-                </thead>
-                <tbody>
-                </tbody>
+                <tr>
+                    <td>이체 날짜</td>
+                    <td>{isParent? '받는 사람' : '보내는 사람'}</td>
+                    <td>이체 메시지</td>
+                    <td>이체 금액</td>
+                </tr>
+                    <tr>
+                        <td>2023/04/11</td>
+                        <td>정길연</td>
+                        <td>4월 용돈</td>
+                        <td>-20,000</td>
+                    </tr>
             </table>
         </div>
     );
