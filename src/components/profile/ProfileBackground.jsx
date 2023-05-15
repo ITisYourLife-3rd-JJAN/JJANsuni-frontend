c286f046fcb6d330b860208cc3ce543b94f4cd3b
gilyeon00 <12venus08@naver.com>
2023-05-15 19:30:36 +0900

    [#18]Design: profileBackground Comp

---
+++ src/components/profile/ProfileBackground.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './profileBackground.css';
+
+const ProfileBackground = () => {
+    return (
+        <div className='profile-back-container'>
+            <img
+                src={`${process.env.PUBLIC_URL}/assets/images/moneystack.png`}
+                alt=''
+                className='profile-background-image-1' 
+            />
+            <img 
+                src={`${process.env.PUBLIC_URL}/assets/images/pigcoin.png`} 
+                alt=""
+                className='profile-background-image-2' 
+            />
+        </div>
+    );
+};
+
+export default ProfileBackground;
--- src/pages/KidProfile.jsx
+++ src/pages/KidProfile.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import ProfileBackground from '../components/profile/ProfileBackground';
 
 const KidProfile = () => {
     return (
         <div>
-            
+            <ProfileBackground/>
         </div>
     );
 };
