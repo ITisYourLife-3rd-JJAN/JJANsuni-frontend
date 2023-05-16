edf996961bf63ea281456618eebd5b40d33fa854
gilyeon <12venus08@naver.com>
2023-05-16 11:14:34 +0900

    [#3]Feat: Join css 수정, Common 컴포넌트 생성

--- src/pages/Join.jsx
+++ src/pages/Join.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './join.css'
+import CommonJoin from '../components/join/CommonJoin';
 
 const Join = () => {
     
- 
+ const [isParent, setIsParent] = useState(true)
+
     return (
         <div className='join'>
             <div className='joinSub'>
@@ -15,44 +17,18 @@ const Join = () => {
                 <div className='joinImage'>
                     <img ClassName="joinParent " src="img/parentJoin.png"></img>
                 </div>
-                
-
-                <div className="selectRoleBox">
-                    <button class="parentBtn">부모 회원입니다.</button>&nbsp;
-                    <button class="kidBtn">자녀 회원입니다.</button><br/><br/><br/><br/><br/>
+         
+                <div className="whiteBackground">
+                    <div className="selectRoleBox">
+                        <button class="parentBtn" onClick={() => setIsParent(true) }>부모 회원입니다</button>&nbsp;
+                        <button class="kidBtn" onClick={() => setIsParent(false)}>자녀 회원입니다</button><br/><br/><br/><br/><br/>
+                    </div>
+                        <CommonJoin isParent={isParent} />
                 </div>
-
-                <div className="joinInput">
-                    
-                    <form action="#" method="post">
-                        
-                        <p>
-                            <label for="username">이름</label><br/><br/>
-                            <input type="text" id="username" required></input>
-                        </p>
-                        <p>
-                            <label for="email">이메일</label><br/><br/>
-                            <input type="email" id="email" required></input>
-                        </p>
-                        <p>
-                            <label for="userpw">비밀번호</label><br/><br/>
-                            <input type="password" id="userpw" required></input>
-                        </p>
-                        <p>
-                            <label for="userpwCheck">비밀번호 확인</label><br/><br/>
-                            <input type="password" id="userpwCheck" required></input>
-                        </p>
-
-                        <p>
-                        <button id="nextBtn">다음</button>
-                        </p>
-                    </form>
-                        <button id="existBtn">중복확인</button>
-                </div>
-
             </div>
-
         </div>
+
+
     );
 };
 
