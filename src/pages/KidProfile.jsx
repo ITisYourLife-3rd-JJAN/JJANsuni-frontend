94c69c1fdcbd49756c16c80a3ae8e60a6cd906fe
gilyeon00 <12venus08@naver.com>
2023-05-16 12:50:58 +0900

    [#18]Feat: StateCard 추가 및 위치 조정

--- src/pages/KidProfile.jsx
+++ src/pages/KidProfile.jsx
@@ -2,13 +2,18 @@ import React from 'react';
 import ProfileBackground from '../components/profile/ProfileBackground';
 import MyBigCard from '../components/profile/MyBigCard';
 import FamCardList from '../components/profile/FamCardList';
+import StateCard from '../components/profile/StateCard';
+import './css/profile.css';
 
 const KidProfile = () => {
     return (
         <div className='profile-cards-container'>
+            <div className='cards-box'> 
+                <MyBigCard/>
+                <StateCard/>
+                <FamCardList/>
+            </div>
             <ProfileBackground/>
-            <MyBigCard/>
-            <FamCardList/>
         </div>
     );
 };
