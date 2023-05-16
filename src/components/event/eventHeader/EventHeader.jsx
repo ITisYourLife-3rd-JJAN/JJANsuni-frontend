e765ab1eda2acde76c62adbb3a4aa1069c2358a4
Subny <richsubin411@gmail.com>
2023-05-16 09:18:19 +0900

    [#23]Feat: 이벤트페이지 배너 1차 publishing

---
+++ src/components/event/eventHeader/EventHeader.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './eventHeader.css';
+
+const EventHeader = () => {
+    return (
+        <div className='eventHeader'>
+            <div className='titleBox'>
+                <img src={`${process.env.PUBLIC_URL}/assets/images/event/이벤트차트.png`} alt='' />
+                <div className='textBox'>
+                    <h1>금주의 이벤트 ! - “투자 성향 파악해보기</h1>
+                    <h3>이벤트를 완료하면 ... 최대 10,000원을 받을 수도.... !</h3>
+                </div>
+                <div>
+                    <form>
+                        
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default EventHeader;
