089b32115b117cad72f7a9556030f536190c2a2c
gilyeon00 <12venus08@naver.com>
2023-05-16 09:48:26 +0900

    [#18]Feat: Create FamCard item

--- src/components/profile/FamCard.jsx
+++ src/components/profile/FamCard.jsx
@@ -5,18 +5,16 @@ const FamCard = () => {
     return (
         <li className='fam-profile-card-container'>
             <img
-                src={`${process.env.PUBLIC_URL}/assets/images/suni.png`}
+                src={`${process.env.PUBLIC_URL}/assets/images/daddy.png`}
                 alt=''
                 className='fam-profile' />
             <div className='fam-info-box'>
-                <p id='fam-name'> 정길연 </p>
-                <p> 좋아하는 것 : 게임 강아지 </p>
-                <p> 꿈 : 연예인 </p>
-                <p> 계좌번호 : 882-602-04182779 </p>
+                <p id='fam-name'> 노승욱 </p>
+                <p> 계좌번호 : 882-655-1432779 </p>
             </div>
             <div className='fam-edit-box'>
-                <p> 탈퇴하기 </p>
-                <p> 회원정보수정 </p>
+                <p> 송금하기 </p>
+                <p> 이체내역 </p>
             </div>
         </li>
         
