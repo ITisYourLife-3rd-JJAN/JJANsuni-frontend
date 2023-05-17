1f6dc0d48b881873b01e57574ea581eefba69952
Subny <richsubin411@gmail.com>
2023-05-17 20:08:24 +0900

    [#23]Fix : Event 페이지 구조 변경

--- src/pages/EventGPT.jsx
+++ src/pages/EventGPT.jsx
@@ -2,19 +2,13 @@ import React from 'react';
 import EventHeader from '../components/event/eventHeader/EventHeader';
 import EventForm from '../components/event/eventForm/EventForm';
 
-const style = {
-    position: 'absolute',
-    right: '0%',
-    bottom: '0%'
-};
+
 
 const EventGPT = () => {
     return (
         <div>
             <EventHeader/>
             <EventForm/>
-            <img src={`${process.env.PUBLIC_URL}/assets/images/event/투자성향확인하러가보기.png`} alt='' style={style}/>
-            <img src={`${process.env.PUBLIC_URL}/assets/images/event/저금통.png`} alt='' style={style}/>
         </div>
     );
 };
