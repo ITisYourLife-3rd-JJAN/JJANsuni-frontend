08fc83db6ff6eef6f754ec9bc0cbe77416f164e9
gilyeon00 <12venus08@naver.com>
2023-05-17 17:23:18 +0900

    [#35]Design: edit Login Page css

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,30 +4,28 @@ import './login.css'
 const Login = () => {
     return (
         <div className='login'>
-            <div class = "loginSub">
-
-                <div className='loginSlogan'>
-                    오늘도 여행을 떠나볼까요?
+            <div className = "loginSub">
+                <div className = "login-container">
+                    <div className='loginSlogan'>
+                        오늘도 여행을 떠나볼까요?
+                    </div>
+                    <div className='loginImage'>
+                        <img className="trip" src="img/trip.png" alt=''/>
+                    </div>
                 </div>
-
-                <div className='loginImage'>
-                    <img ClassName="trip" src="img/trip.png"></img>
-                </div>
-                
 
                 <div className="loginInput">
                     <form action="#" method="post">
-                        <p>
+                            
                             <label for="email">이메일</label><br/><br/>
-                            <input className='loginipt' type="email" id="email" required></input>
-                        </p>
-                        <p>
+                            <input className='loginipt' type="email" id="email" required />
+                        
                             <label for="userpw">비밀번호</label><br/><br/>
-                            <input className='loginipt loginpw' type="password" id="userpw" required></input>
-                        </p>
-                        <p>
+                            <input className='loginipt loginpw' type="password" id="userpw" required />
+                        
+                      
                         <button id="btn2">로그인</button>
-                        </p>
+                        
                     </form>
                 </div>
 
