146055e35a1491208b5e900883d3b8296400d7b1
gilyeon00 <12venus08@naver.com>
2023-05-18 14:24:46 +0900

    [#39]Feat: publishing 오답화면

--- src/pages/SolvedMission.jsx
+++ src/pages/SolvedMission.jsx
@@ -5,24 +5,47 @@ import './css/solvedMission.css'
 
 const SolvedMission = () => {
     const { missionId } = useParams();
+    const status = true;
+
     return (
-        <div className='maplist-container'>
-            <div className='answer-notice-box'>
-                <p>참 잘했어요! 정답은 X 입니다</p>
-            </div>
-            <div className='solution-box'>
-                <div className='sol-left-box'>
-                    <div>
-                        <p className='gpt-btn'>Chat GPT로 해설보기</p>
+        <div>
+        {status ?
+            <div>
+                <div className='maplist-container'>
+                    <div className='answer-notice-box'>
+                        <p>참 잘했어요! 정답은 X 입니다</p>
                     </div>
-                    <img className="thumb-img" src={`${process.env.PUBLIC_URL}/assets/images/thumb.png`} alt=''></img>
+                    <div className='solution-box'>
+                        <div className='sol-left-box'>
+                            <p className='gpt-btn'>Chat GPT로 해설보기</p>
+                            <img className="thumb-img" src={`${process.env.PUBLIC_URL}/assets/images/thumb.png`} alt=''></img>
+                        </div>
+                        <div className='solution-text-box'>
+                            <p>지역금융안정망에 대한 틀린 설명입니다.재원규모와 재원조성 방식, 자금지원 방식,IMF와의 협력, 지원조건 등에 따라 다양한 형태가 있습니다.</p>
+                        </div>
+                    </div>
+                    <MapBackground mapId={missionId}/>
                 </div>
-                <div className='solution-text-box'>
-                    <p>지역금융안정망에 대한 틀린 설명입니다.재원규모와 재원조성 방식, 자금지원 방식,IMF와의 협력, 지원조건 등에 따라 다양한 형태가 있습니다.</p>
+            </div> :
+            <div>
+                <div className='maplist-container'>
+                    <div className='answer-notice-box'>
+                        <p>틀렸어요ㅠㅠ 정답은 X 입니다</p>
+                    </div>
+                    <div className='solution-box'>
+                        <div className='sol-left-box'>
+                            <p className='gpt-btn incorrect'>Chat GPT로 해설보기</p>
+                            <img className="thumb-img" src={`${process.env.PUBLIC_URL}/assets/images/wrong.png`} alt=''></img>
+                        </div>
+                        <div className='solution-text-box'>
+                            <p>지역금융안정망에 대한 틀린 설명입니다.재원규모와 재원조성 방식, 자금지원 방식,IMF와의 협력, 지원조건 등에 따라 다양한 형태가 있습니다.</p>
+                        </div>
+                    </div>
+                    <MapBackground mapId={missionId}/>
                 </div>
             </div>
+            }
             
-            <MapBackground mapId={missionId}/>
         </div>
     );
 };
