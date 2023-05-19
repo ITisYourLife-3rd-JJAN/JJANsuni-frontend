dbd55210cce751706437d7e0dc0909e2b2e51b9e
gilyeon00 <12venus08@naver.com>
2023-05-19 15:35:12 +0900

    [#43]Feat: VOD 시청 화면 구현

---
+++ src/components/Mission/Vod.jsx
@@ -0,0 +1,35 @@
+import { React, useState } from 'react';
+import './vod.css'
+
+const Vod = () => {
+    const [watched, setWatched] = useState(false);
+
+    const handleVideoEnded = () => {
+        setWatched(true);
+    };
+
+    const handleNextButtonClick = () => {
+        // 다음 버튼 클릭 시 동작
+    };
+
+    return (
+        <div className='quiz-container'>
+            <div className='vod-title'>
+                <p>Q. 지역금융안전망(RFAs)는 지리적으로 먼 국가들이 외환보유액 등을 통해 재원을 조성하여 금융, 외환위기에 대응하는 체제이다.</p>
+            </div>
+            <div className='vod-box'>
+                <div className='video-container'>
+                    <video className='vod' src={`${process.env.PUBLIC_URL}/assets/testVideo1.mp4`} onEnded={handleVideoEnded} controls />
+                </div>
+                <button className={watched ? "next-btn" : "next-btn btn-disabled"} onClick={handleNextButtonClick} disabled={!watched}>
+                    완료
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default Vod;
+
+// onEnded 속성은 영상이 종료될 때 handleVideoEnded 함수를 호출
+// controls 속성을 추가 -> 영상 컨트롤을 활성화
