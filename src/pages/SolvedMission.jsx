8fcc1fad3642ef08b874760e84ca6423150c6adb
gilyeon00 <12venus08@naver.com>
2023-05-21 03:11:10 +0900

    [#42]Feat: 미션 완료시, mapList로 이동

--- src/pages/SolvedMission.jsx
+++ src/pages/SolvedMission.jsx
@@ -1,13 +1,15 @@
 import { React, useState, useEffect } from 'react';
 import MapBackground from '../components/Mission/MapBackground';
-import { useParams, useLocation } from 'react-router-dom';
+import { useParams, useLocation, useNavigate } from 'react-router-dom';
 import './css/solvedMission.css'
 import axios from 'axios';
+import confetti from "https://esm.run/canvas-confetti@1";
 
 const SolvedMission = () => {
     const { missionId } = useParams();
-
+    const navigate = useNavigate();
     const location = useLocation();
+    
     const isO = location.state.isO;
     const answer = location.state.answer;
 
@@ -49,6 +51,14 @@ const SolvedMission = () => {
         return <div>Loading...</div>;
     }
 
+    const handleNextButtonClick = () => {
+        confetti({
+            particleCount: 150,
+            spread: 60,
+          });
+        navigate(`/kid/map/${missionId}`);
+    };
+
     return (
         <div>
             {isLoading ?  setIsLoading(true) :
@@ -75,6 +85,10 @@ const SolvedMission = () => {
                             <div className='solution-text-box'>
                                 <p>지역금융안정망에 대한 틀린 설명입니다.재원규모와 재원조성 방식, 자금지원 방식,IMF와의 협력, 지원조건 등에 따라 다양한 형태가 있습니다.</p>
                             </div>
+                            
+                            <button className="next-btn" onClick={handleNextButtonClick}>
+                                완료
+                            </button>
                         </div>
                         <MapBackground mapId={missionId}/>
                     </div>
