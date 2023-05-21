4ed9a6d0a9cf058c1c25714f7b928604e1ab5cba
gilyeon00 <12venus08@naver.com>
2023-05-21 16:24:43 +0900

    [#47]Feat: 사용자의 Achieve 값 화면에 보여주기

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -9,6 +9,8 @@ const Map = () => {
     var mapNum = 1; // 각각의 map-box에 맞게 값을 할당할 변수
 
     const [status, setStatus] = useState();
+    const [preStatus, setPreStatus] = useState(0);
+    const [preMap, setPreMap] = useState(0);
 
     useEffect(() => {
         const getUserAchieve = async () => {
@@ -17,28 +19,52 @@ const Map = () => {
                 .then((response) => {
                     console.log(response.data.data)
                     setStatus(response.data.data.achieve)
+
+                    // setPreStatus(status % 8)
+                    setStatus(16)
+
+                    var quotient = Math.floor(status / 7);
+                    switch (quotient) {
+                    case 0:
+                        setPreMap(1);
+                        break;
+                    case 1:
+                        setPreMap(2);
+                        break;
+                    case 2:
+                        setPreMap(3);
+                        break;
+                    case 3:
+                        setPreMap(4);
+                        break;
+                    case 4:
+                        setPreMap(5);
+                        break;
+                    default:
+                        setPreMap(6);
+                        break;
+                    }
                 })
                 .catch((error) => {
                     console.log(error.response.data);
                 })
         };
-    
+
         getUserAchieve();
+
     }, [status]);
 
-    
 
     return (
         <div className='map-container'>  
             <img
                 src={`${process.env.PUBLIC_URL}/assets/images/map/map_background.png`}
                 alt=''
-                className='map-background-image'
-            />
+                className='map-background-image'/>
             
-            <div className={`map-box map-box-1 ${achieveStatus_full !== achieveStatus_full ? 'gray' : ''}`}>
+            <div className={`map-box map-box-1 ${ preMap < 1 ? 'gray' : ''}`}>
                 <Link to={`/kid/map/${mapNum}`} key={mapNum}>
-                    <p className='achieve-status'> {status}/7</p>
+                    <p className='achieve-status'> {preStatus}/7</p>
                     <img
                     src={`${process.env.PUBLIC_URL}/assets/images/map/island1.png`}
                     alt=''
@@ -48,9 +74,9 @@ const Map = () => {
             </div>
            
 
-            <div className={`map-box map-box-2 ${achieveStatus_full !== achieveStatus_full ? 'gray' : ''}`}>
+            <div className={`map-box map-box-2 ${ preMap < 2 ? 'gray' : ''}`}>
                 <Link to={`/kid/map/${mapNum}`}>
-                    <p className='achieve-status'>{achieveStatus_full}/7</p>
+                    <p className='achieve-status'>{preStatus}/7</p>
                     <img
                     src={`${process.env.PUBLIC_URL}/assets/images/map/island2.png`}
                     alt=''
@@ -59,9 +85,9 @@ const Map = () => {
                 </Link>
             </div>
             
-            <div className={`map-box map-box-3 ${achieveStatus !== achieveStatus_full ? 'gray' : ''}`}>
+            <div className={`map-box map-box-3 ${ preMap < 3 ? 'gray' : ''}`}>
                 <Link to={`/kid/map/3`}>
-                    <p className='achieve-status'>{achieveStatus}/7</p>
+                    <p className='achieve-status'>{preStatus % 7}/7</p>
                     <img
                         src={`${process.env.PUBLIC_URL}/assets/images/map/island3.png`}
                         alt=''
@@ -70,8 +96,8 @@ const Map = () => {
                 </Link>
              </div>
              
-             <div className={`map-box map-box-4 ${achieveStatus !== achieveStatus_full ? 'gray' : ''}`}>
-                <p className='achieve-status'>{achieveStatus}/7</p>
+             <div className={`map-box map-box-4 ${preMap < 4 ? 'gray' : ''}`}>
+                <p className='achieve-status'>{preStatus}/7</p>
                 <img
                     src={`${process.env.PUBLIC_URL}/assets/images/map/island4.png`}
                     alt=''
@@ -79,8 +105,8 @@ const Map = () => {
                 />
              </div>
             
-             <div className={`map-box map-box-5 ${achieveStatus !== achieveStatus_full ? 'gray' : ''}`}>
-                <p className='achieve-status'>{achieveStatus}/7</p>
+             <div className={`map-box map-box-5 ${ preMap < 5 ? 'gray' : ''}`}>
+                <p className='achieve-status'>{preStatus}/7</p>
                 <img
                     src={`${process.env.PUBLIC_URL}/assets/images/map/island5.png`}
                     alt=''
@@ -88,8 +114,8 @@ const Map = () => {
                 />
              </div>
              
-             <div className={`map-box map-box-6 ${achieveStatus !== achieveStatus_full ? 'gray' : ''}`}>
-                <p className='achieve-status'>{achieveStatus}/7</p>
+             <div className={`map-box map-box-6 ${ preMap < 6  ? 'gray' : ''}`}>
+                <p className='achieve-status'>{preStatus}/7</p>
                 <img
                     src={`${process.env.PUBLIC_URL}/assets/images/map/island6.png`}
                     alt=''
