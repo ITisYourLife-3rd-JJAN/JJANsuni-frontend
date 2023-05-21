0ae35b2efd9738d8a22f27a5e699e65baf81976b
gilyeon00 <12venus08@naver.com>
2023-05-21 15:03:54 +0900

    [#47]Feat: Connect 사용자의 미션 현황

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import { React, useState, useEffect } from 'react';
 import './css/map.css'
 import { Link } from 'react-router-dom';
-
+import axios from 'axios';
 
 const Map = () => {
     const achieveStatus_full = 7; // 나중에 실제로 사용할 데이터 변수
     const achieveStatus = 0; // 나중에 실제로 사용할 데이터 변수
     var mapNum = 1; // 각각의 map-box에 맞게 값을 할당할 변수
+
+    const [status, setStatus] = useState();
+
+    useEffect(() => {
+        const getUserAchieve = async () => {
+            await axios
+                .get("http://localhost:8080/api/v1/users/1")
+                .then((response) => {
+                    console.log(response.data.data)
+                    setStatus(response.data.data.achieve)
+                })
+                .catch((error) => {
+                    console.log(error.response.data);
+                })
+        };
+    
+        getUserAchieve();
+    }, [status]);
+
+    
 
     return (
         <div className='map-container'>  
@@ -18,7 +38,7 @@ const Map = () => {
             
             <div className={`map-box map-box-1 ${achieveStatus_full !== achieveStatus_full ? 'gray' : ''}`}>
                 <Link to={`/kid/map/${mapNum}`} key={mapNum}>
-                    <p className='achieve-status'> {achieveStatus_full}/7</p>
+                    <p className='achieve-status'> {status}/7</p>
                     <img
                     src={`${process.env.PUBLIC_URL}/assets/images/map/island1.png`}
                     alt=''
