18fa2bc40f9fe2b8dd984f20850f6ebf6da0c1e8
Subny <richsubin411@gmail.com>
2023-05-25 17:05:54 +0900

    [#23]이벤트 성향 조사 폼 작성 및 계란 꺠기 컴포넌트

--- src/components/event/eventForm/EventForm.jsx
+++ src/components/event/eventForm/EventForm.jsx
@@ -1,177 +1,174 @@
 import React, { useState } from "react";
-import axios from "axios";
 import "./eventForm.css";
+import { useNavigate } from 'react-router-dom';
 
 const EventForm = () => {
-  const [selectedActivity, setSelectedActivity] = useState("");
-  const [selectedSubject, setSelectedSubject] = useState("");
-  const [selectedRoutine, setSelectedRoutine] = useState("");
-  const [selectedWhere, setSelectedWhere] = useState("");
-  const [selectedAmount, setSelectedAmount] = useState("");
-  const [apiResponse, setApiResponse] = useState("");
-  const [loading, setLoading] = useState(false);
+  const subjects = ["국어", "영어", "수학", "사회", "과학", "음악", "미술", "체육"];
+  const initialSubject = subjects[0];
+  const activities = ["독서", "게임", "물놀이", "등산"];
+  const initialActivity = activities[0];
+  const routines = ["나는 매일", "나는 매주", "나는 매월"];
+  const initialRoutine = routines[0];
+  const wheres = [" 게임에", " 교통비에", " 악세사리구매에", " 운동에", " 간식에"];
+  const initialWhere = wheres[0];
 
+  const [selectedActivity, setSelectedActivity] = useState(initialActivity);
+  const [selectedSubject, setSelectedSubject] = useState(initialSubject);
+  const [selectedRoutine, setSelectedRoutine] = useState(initialRoutine);
+  const [selectedWhere, setSelectedWhere] = useState(initialWhere);
+  const [selectedAmount, setSelectedAmount] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
 
-    const saving = document.querySelector(
-      'input[name="saving"]:checked'
-    ).value;
-    const patience = document.querySelector(
-      'input[name="patience"]:checked'
-    ).value;
-    const sociability = document.querySelector(
-      'input[name="sociability"]:checked'
-    ).value;
+    const savingInput = document.querySelector('input[name="saving"]:checked');
+    const patienceInput = document.querySelector('input[name="patience"]:checked');
+    const sociabilityInput = document.querySelector('input[name="sociability"]:checked');
 
-    const requestPrompt = `번호를 매겨서 알려줘
-      ${saving}
+
+    if (!savingInput || !patienceInput || !sociabilityInput || !selectedSubject || !selectedActivity || !selectedRoutine || !selectedWhere || !selectedAmount) {
+      let missingFields = [];
+      if (!savingInput) missingFields.push('어떤 방식을 더 좋아하세요?');
+      if (!patienceInput) missingFields.push('나는 끈기가 있는 편인가요?');
+      if (!sociabilityInput) missingFields.push('어떻게 시간을 보내는 것을 좋아하나요?');
+      if (!selectedSubject) missingFields.push('좋아하는 과목은 무엇인가요?');
+      if (!selectedActivity) missingFields.push('취미가 무엇인가요?');
+      if (!selectedRoutine) missingFields.push('나는 매일/매주/매월');
+      if (!selectedWhere) missingFields.push('어디에');
+      if (!selectedAmount) missingFields.push('원 쓰는데 나는 무슨 성향이야??');
+  
+      alert(`선택하지 않은 항목이 있어요😲\n\n${missingFields.join('\n')}`);
+      setLoading(false);
+      return;
+    }
+
+const saving = savingInput.value;
+const patience = patienceInput.value;
+const sociability = sociabilityInput.value;
+
+    const requestPrompt = `${saving}
       ${patience}
       ${sociability}
       학교에서 ${selectedSubject}시간을 좋아해
       ${selectedActivity} 하는 것을 좋아해
       ${selectedRoutine} ${selectedWhere} ${selectedAmount}원 소비하는데
-      어떤 방식으로 투자나 저축을 하면 좋을까?`;
+      어떤 방식으로 투자나 저축을 하면 좋을까? 참고로 난 이전 사람과 다른 사람이야`;
 
-    try {
-        setLoading(true);
-      const response = await axios.post(
-        "https://api.openai.com/v1/engines/text-davinci-003/completions",
-        {
-          prompt: requestPrompt,
-          temperature: 0.5,
-          max_tokens: 3000,
-        },
-        {
-          headers: {
-            Authorization: `Bearer ${process.env.REACT_APP_OPENAI_API_KEY}`,
-            "Content-Type": "application/json",
-          },
-        }
-      );
-      setApiResponse(response.data.choices[0].text);
-    } catch (e) {
-      console.error(e);
-      setApiResponse("Something is going wrong. Please try again.");
-    }
-    setLoading(false);
-  };
-    
-    return (
-        <>
-        <div className='eventForm'>
-            <div className='formBox'>            
-            <form>
-                <div className="question">
-                    어떤 방식을 더 좋아하세요?<br/>
-                    <input type="radio" id="bigSave" name="saving" value="나는 돈을 모아서, 갖고 싶은 비싼 물품을 사는 것을 추구해"
-                    /> 
-                    <label htmlFor="bigSave">돈을 모아서 갖고 싶은 비싼 물건 사기</label><br/>
-                    <input type="radio" id="smallSave" name="saving" value="나는 한 번 큰 것을 사는 것보다, 매일매일 조금씩 구매하는게 좋아"
-                    /> 
-                    <label htmlFor="smallSave">조금씩 자주 저렴한 물건 사기</label>
-                </div>
+      alert(requestPrompt);
+      navigate('/event-result', { state: { requestPrompt } });
+    };
+  
 
-                <div className="question">
-                    나는 끈기가 있는 편인가요?<br/>
-                    <input type="radio" id="yesPatience" name="patience" value="나는 끈기가 있는 편에 속해"
-                    /> 
-                    <label htmlFor="yesPatience">예</label>
-                    <input type="radio" id="noPatience" name="patience" value="나는 끈기가 없는 편에 속해"
-                    /> 
-                    <label htmlFor="noPatience">아니오</label>
-                </div>
-
-                <div className="question">
-                    어떻게 시간을 보내는 것을 좋아하나요?<br/>
-                    <input type="radio" id="extrovert" name="sociability" value="나는 혼자서 노는게 좋아"
-                    /> 
-                    <label htmlFor="extrovert">혼자 시간을 보내는 것을 좋아해요</label><br/>
-                    <input type="radio" id="introvert" name="sociability" value="나는 친구들과 노는게 좋아"
-                    /> 
-                    <label htmlFor="introvert">친구들과 시간을 보내는 것을 좋아해요</label>
-                </div>
-
-                <div className="question">
-                좋아하는 과목은 무엇인가요?
-                    <select
-                        id="subject-select"
-                        value={selectedSubject}
-                        onChange={(e) => setSelectedSubject(e.target.value)}
-                        >
-                        <option value="국어">국어</option>
-                        <option value="영어">영어</option>
-                        <option value="수학">수학</option>
-                        <option value="사회">사회</option>
-                        <option value="과학">과학</option>
-                        <option value="음악">음악</option>
-                        <option value="미술">미술</option>
-                        <option value="체육">체육</option>
-                    </select>
-                </div>
-                <div className="question">
-                취미가 무엇인가요?
-                    <select
-                    id="activity-select"
-                    name="activity"
-                    value={selectedActivity}
-                    onChange={(e) => setSelectedActivity(e.target.value)}
-                    >
-                    <option value="독서">독서</option>
-                    <option value="게임">게임</option>
-                    <option value="산">등산</option>
-                    <option value="계곡">물놀이</option>
-                    </select>
-                </div>
-                <div className="question">
-                    나는 매
-                    <select
-                    id="routine-select"
-                    value={selectedRoutine}
-                    onChange={(e) => setSelectedRoutine(e.target.value)}
-                    >
-                    <option value="나는 매일">일</option>
-                    <option value="나는 매주">주</option>
-                    <option value="나는 매월">월</option>
-                    </select>
-
-                    <select
-                    id="where-select"
-                    value={selectedWhere}
-                    onChange={(e) => setSelectedWhere(e.target.value)}
-                    >
-                    <option value=" 게임에">게임</option>
-                    <option value=" 교통비에">교통비</option>
-                    <option value=" 덕질에">덕질</option>
-                    </select>
-                    &nbsp;에<br/>
-                    <input type="number" id="howmuch" placeholder="숫자만 입력하세요"value={selectedAmount}
-                    onChange={(e) => setSelectedAmount(e.target.value)}/>원
-                    쓰는데 나는 무슨 성향이야??
-                </div>
-            </form>
+  return (
+    <>
+      <div className='eventForm'>
+        <div className='formBox'>            
+          <form>
+            <div className="question">
+              어떤 방식을 더 좋아하세요?<br/>
+              <input type="radio" id="bigSave" name="saving" value="나는 돈을 모아서, 갖고 싶은 비싼 물품을 사는 것을 추구해" required /> 
+              <label htmlFor="bigSave">돈을 모아서 갖고 싶은 비싼 물건 사기</label><br/>
+              <input type="radio" id="smallSave" name="saving" value="나는 한 번 큰 것을 사는 것보다, 매일매일 조금씩 구매하는게 좋아" required /> 
+              <label htmlFor="smallSave">조금씩 자주 저렴한 물건 사기</label>
             </div>
-            {/* submit 버튼 만들어야함 */}
+
+            <div className="question">
+              나는 끈기가 있는 편인가요?<br/>
+              <input type="radio" id="yesPatience" name="patience" value="나는 끈기가 있는 편에 속해" required /> 
+              <label htmlFor="yesPatience">예</label>
+              <input type="radio" id="noPatience" name="patience" value="나는 끈기가 없는 편에 속해" required /> 
+              <label htmlFor="noPatience">아니오</label>
+            </div>
+
+            <div className="question">
+              어떻게 시간을 보내는 것을 좋아하나요?<br/>
+              <input type="radio" id="extrovert" name="sociability" value="나는 혼자서 노는게 좋아" required /> 
+              <label htmlFor="extrovert">혼자 시간을 보내는 것을 좋아해요</label><br/>
+              <input type="radio" id="introvert" name="sociability" value="나는 친구들과 노는게 좋아" required /> 
+              <label htmlFor="introvert">친구들과 시간을 보내는 것을 좋아해요</label>
+            </div>
+
+            <div className="question">
+              좋아하는 과목은 무엇인가요?
+              <select
+                id="subject-select"
+                value={selectedSubject}
+                onChange={(e) => setSelectedSubject(e.target.value)}
+              >
+                <option value="국어" >국어</option>
+                <option value="영어">영어</option>
+                <option value="수학">수학</option>
+                <option value="사회">사회</option>
+                <option value="과학">과학</option>
+                <option value="음악">음악</option>
+                <option value="미술">미술</option>
+                <option value="체육">체육</option>
+              </select>
+            </div>
+
+            <div className="question">
+              취미가 무엇인가요?
+              <select
+                id="activity-select"
+                name="activity"
+                value={selectedActivity}
+                onChange={(e) => setSelectedActivity(e.target.value)}
+                required
+              >
+                <option value="독서" >독서</option>
+                <option value="게임">게임</option>
+                <option value="산">등산</option>
+                <option value="계곡">물놀이</option>
+              </select>
+            </div>
+
+            <div className="question">
+              나는 매
+              <select
+                id="routine-select"
+                value={selectedRoutine}
+                onChange={(e) => setSelectedRoutine(e.target.value)}
+                required
+              >
+                <option value="나는 매일" >일</option>
+                <option value="나는 매주">주</option>
+                <option value="나는 매월">월</option>
+              </select>
+
+              <select
+                id="where-select"
+                value={selectedWhere}
+                onChange={(e) => setSelectedWhere(e.target.value)}
+                required
+              >
+                <option value=" 게임에" >게임</option>
+                <option value=" 교통비에">교통비</option>
+                <option value=" 덕질에">덕질</option>
+                <option value=" 악세사리구매에">악세사리구매</option>
+                <option value=" 운동에">운동</option>
+                <option value=" 간식에">간식</option>
+              </select>
+              &nbsp;에<br/>
+              <input type="number" id="howmuch" placeholder="숫자만 입력하세요" value={selectedAmount}
+                onChange={(e) => {
+                  const value = parseInt(e.target.value);
+                  setSelectedAmount(value >= 1 ? value : "");
+                }} required />
+              원 쓰는데 나는 무슨 성향이야??
+            </div>
+
             <div className="submitBtn" onClick={handleSubmit}>
-        <img src={`${process.env.PUBLIC_URL}/assets/images/event/letsgo.png`} alt='' className="letsgo"  style={{zIndex:100}}/>
-        <img src={`${process.env.PUBLIC_URL}/assets/images/event/piggy.png`} alt='' className="letsgo"/></div>
-        {/* API 응답 */}
-      {loading ? (
-        <div>Loading...</div>
-      ) : (
-        apiResponse && (
-          <div>
-            <pre>
-              <strong>ChatGPT의 답변</strong><br/>
-              {apiResponse}
-            </pre>
-          </div>
-        )
-      )}
-    </div>
+              <img src={`${process.env.PUBLIC_URL}/assets/images/event/letsgo.png`} alt='' className="letsgo" style={{ zIndex: 100 }} />
+              <img src={`${process.env.PUBLIC_URL}/assets/images/event/piggy.png`} alt='' className="letsgo" />
+            </div>
+          </form>
+        </div>
+      </div>
     </>
   );
 };
+
 export default EventForm;
--- src/components/event/eventResult/EggBreak.jsx
+++ src/components/event/eventResult/EggBreak.jsx
@@ -1,69 +1,92 @@
+import axios from 'axios';
 import React, { useState } from 'react';
 
 const EggBreak = () => {
   const [count, setCount] = useState(20);
   const [lock, setLock] = useState(true);
   const [giftNum, setGiftNum] = useState(null);
-  const [addressVisible, setAddressVisible] = useState(false);
+  const [showButton, setShowButton] = useState(false);
+  const [isButtonDisabled, setIsButtonDisabled] = useState(false); // 버튼 상태를 관리하기 위한 상태 추가
 
-  const imgArray = [
-    `${process.env.PUBLIC_URL}/assets/images/map/island1.png`,
-    `${process.env.PUBLIC_URL}/assets/images/map/island2.png`,
-    `${process.env.PUBLIC_URL}/assets/images/map/island3.png`
-  ];
-  
 
   const textArray = [
-    "1",
-    "10",
-    "3",
-    "7",
-    "1000",
-    "10000"
+    "1","10","3","7","50","700","1400","3600","600","1000","10000"
   ];
 
   const hit = () => {
     if (lock) {
-      if (count < 5) {
-        document.getElementById("wallet").src = `${process.env.PUBLIC_URL}/assets/images/map/island4.png`;
-    } else if (count < 12) {
-        document.getElementById("wallet").src = `${process.env.PUBLIC_URL}/assets/images/map/island5.png`;
-      } else if (count < 20) {
-        document.getElementById("wallet").src = `${process.env.PUBLIC_URL}/assets/images/map/island6.png`;
-      }
 
       if (count > 1) {
         setCount(count - 1);
       } else {
-        const randomNum = Math.floor(Math.random() * 6);
+        const randomNum = Math.floor(Math.random() * 11);
         setGiftNum(randomNum);
         setLock(false);
+        setShowButton(true);
       }
     }
   };
+    
+      const getEggImage = () => {
+        if (count < 2) {
+          return `${process.env.PUBLIC_URL}/assets/images/event/egg4.png`;
+        }else if(count <5){
+          return `${process.env.PUBLIC_URL}/assets/images/event/egg3.png`;
+        }else if (count < 12) {
+          return `${process.env.PUBLIC_URL}/assets/images/event/egg2.png`;
+        } else if (count < 20) {
+          return `${process.env.PUBLIC_URL}/assets/images/event/egg1.png`;
+        }
+        else {
+          return `${process.env.PUBLIC_URL}/assets/images/event/egg.png`;
+        }
+      };
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
-    // handle form submission logic here
+    axios
+    .patch('http://localhost:8080/api/v1/debits/game',{
+      userId : 1,
+      price : textArray[giftNum],
+    })
+    .then((response)=>{
+      console.log(response);
+      alert("당첨금 "+textArray[giftNum]+"원 지급 완료!🤩")
+      setIsButtonDisabled(true); // 버튼을 비활성화
+    })
+    .catch((error)=>{
+      console.log(error)
+    });
   };
 
+  let giveComplete;
+  if (isButtonDisabled) {
+    giveComplete = "당첨금 지급완료";
+  } else {
+    giveComplete = "당첨금 받기";
+  }
+
   return (
-    <div className="content">
+  <>
+    <div className="eggContent">
       <div id="information" style={{fontSize:"45px", padding:"50px"}}> 
-  {lock ? "지갑을 클릭해주세요!" : giftNum !== null ? `축하합니다! ${textArray[giftNum]} 원 당첨!` : ""}
-</div>
+  {lock ? "알을 깨주세요!!" : giftNum !== null ? `축하합니다! ${textArray[giftNum]} 원 당첨!` : ""}
+    </div>
 
       <br />
       <div id="show_count">{lock ? count : null}</div>
-      <button type="button" onClick={hit} id="hit_btn">
-        <img src={`${process.env.PUBLIC_URL}/assets/images/wallet.png`} style={{ width: '500px' }} id="wallet" alt="wallet" />
-      </button>
-      <br />
-      <form id="hiddenevent" action="" onSubmit={handleFormSubmit}>
-        <input type="text" id="address" placeholder="CIFY를 받을 지갑주소를 입력하시오" style={{ width: '350px', height: '25px' }} />
-        <button type="submit" id="submit" style={{ fontSize: '17px' }}>받기</button>
-      </form>
+      <div className="eggCharge">
+        <img src={getEggImage()} className="eggImg" alt="wallet" onClick={hit}/>
+        
+      </div>
+      {giftNum !== null && showButton && (
+            <form id="hiddenevent" action="" onSubmit={handleFormSubmit}>
+              <button type="submit" className="eggSubmit" style={{ fontSize: '17px' }}
+              disabled={isButtonDisabled}>{giveComplete}</button>
+            </form>
+        )}
     </div>
+    </>
   );
 };
 
