f1ad1d990326463ea6f99d3e8be4d7f547f20ccc
seungukNoh <dnrdl8255@gmail.com>
2023-05-25 14:39:11 +0900

    (#69) add Logout and sessionStorage in header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import './header.css'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate  } from 'react-router-dom';
 
 const Header = () => {
+    const navigate = useNavigate ();
+    const Logout = () => {
+        sessionStorage.removeItem('userId');
+        sessionStorage.removeItem('username');
+        sessionStorage.removeItem('isParent');
+        navigate('./login');
+    }
+    const role = (sessionStorage.getItem('isParent')==="T") ? true : false;
+
     return (
         <div className='header'>
             <p>JJAN순이의 경제여행</p>
-            <Link to = {"/kid/profile"}><p>정길연 아이님✨</p></Link>
-            <p className='logout'>로그아웃</p>
+            {role && <Link to = {"/parent/profile"}><p>{sessionStorage.getItem('username')}부모님✨</p></Link>}
+            {!role && <Link to = {"/kid/profile"}><p>{sessionStorage.getItem('username')}아이님✨</p></Link>}
+            
+            <p className='logout' onClick={Logout}>로그아웃</p>
         </div>
     );
 };
-
 export default Header;
