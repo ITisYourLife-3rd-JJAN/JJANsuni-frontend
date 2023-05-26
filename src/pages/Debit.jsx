28fcfb98b8cd838077e47fb26aa865bf5ef39173
Subny <richsubin411@gmail.com>
2023-05-26 17:07:46 +0900

    [73]FEAT : kidUserId 값 불러오도록 설정

--- src/pages/Debit.jsx
+++ src/pages/Debit.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import DebitBanner from '../components/debitBanner/DebitBanner';
 import DebitForm from '../components/debitForm/DebitForm';
 import Header from '../components/header/Header';
 
 const Debit = () => {
+    const [kidUserId, setKidUserId] = useState();
+    const [kidUserName, setKidUserName] = useState();
     return (
         <div style={{display: 'flex', justifyContent: 'center'}}>
             <Header/>
-            <DebitBanner idx="1" color='#E5FAFC'/>
-            <DebitForm/>
+            <DebitBanner idx="1" color='#E5FAFC' setKidUserId={setKidUserId} setKidUserName={setKidUserName}/>
+            <DebitForm kidUserId={kidUserId} kidUserName={kidUserName}/>
         </div>
     );
 };
