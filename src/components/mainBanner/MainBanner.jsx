d8d02799726847593064e08922f4f27a5b15be30
gilyeon00 <12venus08@naver.com>
2023-05-29 11:15:55 +0900

    [#81]Feat: 부모 Main Banner에 아이 Select 구현

--- src/components/mainBanner/MainBanner.jsx
+++ src/components/mainBanner/MainBanner.jsx
@@ -4,11 +4,16 @@ import SimpleSlider from '../slider/Slider';
 import { useNavigate } from "react-router-dom";
 import axios from 'axios';
 import { checkFinalSound } from '../../lib/checkFinalSound';
+import Select from 'react-select'
 
 const MainBanner = ({ bgColor = '#CDFF5C', isParent }) => {
 
   const [cheerUpMsg, setCheerUpMsg] = useState("");
   const [name, setName] = useState("");
+  const [children, setChildren] = useState([]); 
+  const [selectedChild, setSelectedChild] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+    
   const userId = sessionStorage.getItem("userId");
 
   const navigate = useNavigate();
@@ -20,17 +25,17 @@ const MainBanner = ({ bgColor = '#CDFF5C', isParent }) => {
   const cursorStyle = isParent ? { cursor: 'pointer' } : {};
   const backgroundColor = isParent ? 'rgba(252,255,92,0.4)' : null;
 
-  const getEnding = () => {
-    const lastTwoChars = name.slice(-2); 
+  const getEnding = (text) => {
+    const lastTwoChars = text.slice(-2); 
     
-    if(checkFinalSound(name)) return `${lastTwoChars}야`;
+    if(checkFinalSound(text)) return `${lastTwoChars}야`;
     else return `${lastTwoChars}아`;
   };
 
 
   useEffect(() => {
     const fetchData = () => {
-      if (isParent === "F") {
+      if (!isParent) {
         axios
           .get(`http://localhost:8080/api/v1/users/${userId}`)
           .then((response) => {
@@ -45,8 +50,15 @@ const MainBanner = ({ bgColor = '#CDFF5C', isParent }) => {
         axios
           .get(`http://localhost:8080/api/v1/users/family-list/${userId}`)
           .then((response) => {
-            console.log(response.data.data);
-          })
+            const filteredChildren = response.data.data.filter(
+                (child) => child.isParent === "F"
+              );
+              setChildren(filteredChildren); 
+              setIsLoading(false);
+            })
+        .catch((error) => {
+            console.log(error);
+        })
           .catch((error) => {
             console.log(error.response.data);
           });
@@ -57,22 +69,75 @@ const MainBanner = ({ bgColor = '#CDFF5C', isParent }) => {
 
   }, [isParent, userId]);
 
+  const handleChildSelect = (selectedOption) => {
+    setSelectedChild(selectedOption.label);
+  };
 
 
   return (
     <div className='mainBanner' style={{ backgroundColor: bgColor }}>
+      {isParent ? (
+        <Select
+          className="selectkid"
+          onChange={handleChildSelect}
+          options={children.map((child) => ({
+              value: child.userId,
+              label: child.name
+          }))}
+          placeholder={children.length > 0 ? children[0].name : "아이를 등록해주세요"}
+          theme={(theme) => ({
+              ...theme,
+              colors: {
+                ...theme.colors,
+                primary25: '#E5FAFC',
+                primary: '#CDFF5C',
+              },
+            })} 
+          styles={{
+              control: (provided, state) => ({
+                ...provided,
+                borderColor: state.isFocused ? "#CDFF5C" : "#CDFF5C",
+                borderRadius: "10px", 
+                boxShadow: "none",
+                "&:hover": {
+                  borderColor: "#CDFF5C"
+                }
+              }),
+              menu: (provided) => ({
+                ...provided,
+                marginTop: 0
+              }),
+              option: (provided) => ({
+                ...provided,
+                color: "black"
+              })
+            }}
+          />
+        ):("")
+      }
+
       <div className='mammyMsg'>
-        {isParent && (
+        {isParent ? (
+          children.length > 0 ? (
+            <div>
+              <p className='sendFighting' onClick={handleClick}>
+                  작성하기
+              </p>
+              <p className='changeChild' onClick={isParent ? handleClick : null} style={{...cursorStyle, backgroundColor}}>
+                사랑하는 {getEnding(selectedChild)} ~
+              </p>
+              <p>{cheerUpMsg}</p>
+            </div>
+          ) :  ("아이를 등록해주세요")
+        ) : (
           <div>
-            <p className='sendFighting' onClick={handleClick}>
-                작성하기
-            </p>
+              <p className='changeChild' onClick={isParent ? handleClick : null} style={{...cursorStyle, backgroundColor}}>
+              사랑하는 {getEnding()} ~
+              </p>
+              <p>{cheerUpMsg}</p>
           </div>
         )}
-        <p className='changeChild' onClick={isParent ? handleClick : null} style={{...cursorStyle, backgroundColor}}>
-          사랑하는 {getEnding()} ~
-        </p>
-        <p>{cheerUpMsg}</p>
+       
         
       </div>
       <div className='slideBanner'>
